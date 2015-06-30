e01c17e13465389cd1ab533887b635976778a13e
Brian Graham <graham.briank@gmail.com>
2015-06-30 11:04:54 -0700

    Write Extend and Defaults

--- src/underbar.js
+++ src/underbar.js
@@ -194,7 +194,7 @@
       return !iterator(item);
     }); */
 
-    /** With a loop */
+    /** With a loop (faster than with Every, stops when it reaches a true statement */
     var len = collection.length;
     for (var i = 0; i < len; i++){
       if (iterator(collection[i])) return true;
@@ -222,11 +222,23 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(item){
+      _.each(item, function(value, key){ // remember _.each takes value, key, collection (order)
+        obj[key] = value;
+      });
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(item){
+      _.each(item, function(value, key){
+        if (typeof obj[key] === 'undefined') {obj[key] = value;}
+      });
+    });
+    return obj;
   };
 
 
